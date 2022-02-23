e0f7e667a825fb587097aacfe647ca3dbacd4f12
Diego Rangel <starloss09@gmail.com>
2022-02-23 02:38:01 -0400

    Removed import unnecesary

--- contracts/ERC721_Token.sol
+++ contracts/ERC721_Token.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
