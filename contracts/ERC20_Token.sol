6726fbab211d7a78f4fcef54137f2046be8afb37
Diego Rangel <starloss09@gmail.com>
2022-02-24 02:17:40 -0400

    Added the function to whitdraw ether in ERC20 Added comments

--- contracts/ERC20_Token.sol
+++ contracts/ERC20_Token.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Token is ERC20 {
+contract ERC20Token is ERC20, Ownable {
     /// VARIABLES
     uint cost = 0.001 ether;
     /**
@@ -19,12 +20,28 @@ contract ERC20Token is ERC20 {
         _mint(msg.sender, _supply);
     }
 
+    /**
+     *  @notice Function that allows mint the token
+     *  @param _mintAmount is the amount of tokens to be minted
+     */
     function mint(uint _mintAmount) payable public {
         require(msg.value >= cost * _mintAmount, "Insufficient funds!");
         _mint(msg.sender, _mintAmount);
     }
 
+    /**
+     *  @notice Function that overrides the ERC20 decimals to set decimals in 0
+     *  @notice This sets the min quantity of tokens in 1
+     */
     function decimals() public view virtual override(ERC20) returns (uint8) {
         return 0;
+    }
+
+    /**
+     *  @notice Function that allow the owner to withdraw all the funds
+     */
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner().call{value: address(this).balance}("");
+        require(success);
     }
 }
